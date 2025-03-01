659d8cdf7d7cc46af8e937810f7d52af74141186
Jose <jose0lmedojr@gmail.com>
2025-03-01 01:12:58 -0600

    add user prompt

--- main.c
+++ main.c
@@ -2,35 +2,51 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <ncurses.h>
+#include <string.h>
 
 #define BUFFER_SIZE 1024
+#define MAX_CMD_LEN 256
 
 void setup_ncurses();
 void cleanup_ncurses();
-void read_pipe_and_display(int fd);
-void run_child_process(int pipe_fd[2]);
+void display_output(int fd);
+void run_child_process(int pipe_fd[2], char *command);
 void run_parent_process(int pipe_fd[2]);
+void get_user_command(char *cmd_buffer);
 
 int main() {
-    int pipe_fd[2];
+    char command[MAX_CMD_LEN];
 
-    if (pipe(pipe_fd) == -1) {
-        perror("pipe failed");
-        return 1;
+    setup_ncurses();
+
+    while(1) {
+        get_user_command(command);
+        
+        if (strcmp(command, "exit") == 0) {
+            break;
+        }
+        
+        int pipe_fd[2];
+        if (pipe(pipe_fd) == -1) {
+            perror("pipe failed");
+            return 1;
+        }
+
+        pid_t pid = fork();
+        if (pid == -1) {
+            perror("fork failed");
+            return 1;
+        }
+        
+        if (pid == 0) {
+            run_child_process(pipe_fd, command);
+        } else {
+            close(pipe_fd[1]);
+            display_output(pipe_fd[0]);
+            close(pipe_fd[0]);
+        }
     }
-
-    pid_t pid = fork();
-    if (pid == -1) {
-        perror("fork failed");
-        return 1;
-    }
-
-    if (pid == 0) {  
-        run_child_process(pipe_fd);
-    } else {  
-        run_parent_process(pipe_fd);
-    }
-
+    cleanup_ncurses();
     return 0;
 }
 
@@ -42,11 +58,13 @@ void setup_ncurses() {
 }
 
 void cleanup_ncurses() {
+    mvprintw(LINES -1, 0, "Press any key to exit...");
+    refresh();
     getch();    
     endwin();   
 }
 
-void read_pipe_and_display(int fd) {
+void display_output(int fd) {
     char buffer[BUFFER_SIZE];
     ssize_t bytes_read;
 
@@ -57,7 +75,7 @@ void read_pipe_and_display(int fd) {
     }
 }
 
-void run_child_process(int pipe_fd[2]) {
+void run_child_process(int pipe_fd[2], char *command) {
     close(pipe_fd[0]);  
 
     if (dup2(pipe_fd[1], STDOUT_FILENO) == -1) {
@@ -72,18 +90,17 @@ void run_child_process(int pipe_fd[2]) {
 
     close(pipe_fd[1]);  
 
-    execlp("ls", "ls", "-l", NULL);  
+    execlp("/bin/sh", "sh", "-c", command, NULL);  
     perror("execlp failed");
     exit(1);
 }
 
-void run_parent_process(int pipe_fd[2]) {
-    close(pipe_fd[1]);  
-
-    setup_ncurses();
-    read_pipe_and_display(pipe_fd[0]);  
-    cleanup_ncurses();
-
-    close(pipe_fd[0]);  
+void get_user_command(char *cmd_buffer) {
+    mvprintw(LINES - 1, 0, "Enter command: ");
+    echo();
+    getnstr(cmd_buffer, MAX_CMD_LEN -1);
+    noecho();
+    move(LINES -1, 0);
+    clrtoeol();
 }
 
